afd0151700d4241b1c032436dd71c0ea2e103f2c
unknown <product.development@jesperapps.com>
2023-12-16 19:51:16 +0530

    code src, pom.xml

---
+++ src/main/java/com/test/cicd/CicdApplication.java
@@ -0,0 +1,21 @@
+package com.test.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to CICD";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
